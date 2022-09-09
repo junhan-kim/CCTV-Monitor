99ff49734be5394bb3734525990470a878f2f738
junhan <kjune1236@gmail.com>
2022-09-09 17:33:57 +0900

    remove exception

--- backend/app.py
+++ backend/app.py
@@ -36,14 +36,10 @@ def root():
 @app.route('/stream/start', methods=['POST'])
 def start_stream():
     # get_params
-    try:
-        params = request.get_json()
-        logger.info(f'params: {params}')
-        channel_name = params['channelName']  # HLS connection name
-        source_url = params['sourceUrl']  # source youtube url
-    except KeyError:
-        logger.error('Invalid Param in /stream/start')
-        traceback.print_exc()
+    params = request.get_json()
+    logger.info(f'params: {params}')
+    channel_name = params['channelName']  # HLS connection name
+    source_url = params['sourceUrl']  # source youtube url
 
     # set streamer
     try:
@@ -70,12 +66,8 @@ def start_stream():
 @app.route('/stream/stop', methods=['POST'])
 def stop_stream():
     # get_params
-    try:
-        params = request.get_json()
-        channel_name = params['channelName']
-    except KeyError:
-        logger.error('Invalid Param in /stream/stop')
-        traceback.print_exc()
+    params = request.get_json()
+    channel_name = params['channelName']
 
     # stop streamer
     try:
