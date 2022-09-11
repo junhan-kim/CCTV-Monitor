b87fe8df171146e39d1bc30c260fe9d0f0fd1db1
junhan <kjune1236@gmail.com>
2022-09-11 18:33:35 +0900

    manage channel names with bidict instead of redis

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ import time
 import traceback
 from configparser import ConfigParser
 
+import bidict
 import redis
 from flask import Flask, jsonify, render_template, request, make_response
 from flask_cors import CORS
@@ -27,7 +28,8 @@ rd = redis.Redis(host='redis', port=6379, db=0, charset="utf-8", decode_response
 
 # set default params
 dest_url = "rtmp://media_server/live"  # rtmp + application name
-streamers = {}
+streamers = {}  # key: channel_name, value: streamer process
+channel_names = bidict.bidict({})  # key: source_url, value: channel_name
 
 
 @app.route('/', methods=['GET'])
@@ -44,18 +46,21 @@ def start_stream():
 
     # set streamer
     try:
-        if not rd.exists(source_url):  # 해당 URL에 대한 Streamer가 없다면 생성
+        if source_url not in channel_names:  # 해당 URL에 대한 Streamer가 없다면 생성
             channel_name = generate_id_by_uuid()
-            logger.info(f'source_url key not exist in redis. start stream with {channel_name}')
+            logger.info(f'source_url key not exist. start stream with {channel_name}')
             streamer = Streamer(source_url=source_url, dest_url=f'{dest_url}/{channel_name}',
                                 youtube_api_key=youtube_api_key)
             streamer.start()
             time.sleep(20)  # index.m3u8 생기는데까지 걸리는 지연시간 부여
-            rd.set(source_url, channel_name)
+            channel_names[source_url] = channel_name
             streamers[channel_name] = streamer
 
-        channel_name = str(rd.get(source_url))  # 해당 URL에 대한 채널명으로 응답
+        channel_name = str(channel_names[source_url])  # 해당 URL에 대한 채널명으로 응답
         logger.info(f'source_url: {source_url} -> channel_name: {channel_name}')
+
+        logger.info(f'streamers: {streamers}')
+        logger.info(f'channel_names: {channel_names}')
 
     except Exception:
         logger.error('Error start stream from server.')
@@ -81,7 +86,14 @@ def stop_stream():
         streamer.stop_video_stream()
         streamer.join()
         streamers.pop(channel_name)
+        del channel_names.inverse[channel_name]  # delete by value with bidict
+
+        logger.info(f'streamers: {streamers}')
+        logger.info(f'channel_names: {channel_names}')
+
     except Exception:
+        logger.error('Error stop stream from server.')
+        traceback.print_exc()
         return make_response("Error stop stream", 500)
     return make_response("Success stop stream", 200)
 
