430bfefd57cc7992519fd0f55ef626c8fdda7d6b
junhan <kjune1236@gmail.com>
2022-09-12 22:19:07 +0900

    to python alpine

--- media_server/stream_manager.sh
+++ media_server/stream_manager.sh
@@ -4,7 +4,14 @@
 ACCESS_LOG_PATH=./logs/access.log
 
 recent_access_log=$(tail -n 100 $ACCESS_LOG_PATH)
-# echo $recent_access_log
-channel_names=$(grep -oP "[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}" <<< $recent_access_log)
 
-echo $channel_names
+while IFS= read -r line; do
+    time=$(grep -oP "" <<< $line)
+    channel_names=$(grep -oP "[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}" <<< $line)
+    echo "Text read from file: $channel_names"
+done < $recent_access_log
+
+# echo $recent_access_log
+# channel_names=$(grep -oP "[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}" <<< $recent_access_log)
+
+# echo $channel_names
